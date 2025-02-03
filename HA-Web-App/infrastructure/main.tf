8fde1ff4524113cd186836b77633b15f443016e7
kodcapsule <kusehsw@gmail.com>
2025-02-03 23:23:02 +0000

    configured remote state and added .gitignore file

---
+++ HA-Web-App/infrastructure/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    aws={
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+    }
+  }
+   required_version = ">= 1.2.0"
+
+   backend "s3" {
+    bucket = "kodecapsule-state"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "state-lock-table"
+    encrypt = true
+     
+   }
+}
+
+
+provider "aws" {
+    region = "us-east-1"  
+}
+
+
+
+resource "aws_s3_bucket" "state-bucket" {
+    bucket = "kodecapsule-state"
+    lifecycle {
+      prevent_destroy = true
+    }     
+}
+
+resource "aws_s3_bucket_versioning" "state-version" {
+    bucket = aws_s3_bucket.state-bucket.id 
+    versioning_configuration {
+      status = "Enabled"
+  }
+  
+}
+
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket-encryption" {
+    bucket = aws_s3_bucket.state-bucket.id
+
+    rule {
+        apply_server_side_encryption_by_default {
+           sse_algorithm     = "AES256"
+        }    
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "sublic-access" {
+    bucket = aws_s3_bucket.state-bucket.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+
+}
+
+
+resource "aws_dynamodb_table" "terrafor-state-lock" {
+    name = "state-lock-table"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- HA-Web-App/infrastructure/modules/VPC/main.tf
+++ HA-Web-App/infrastructure/modules/VPC/main.tf
@@ -1,8 +1,64 @@
+
+
+# ============ VPC =====================================
 resource "aws_vpc" "kodecapsule-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-
   tags = {
     Name = "kodecapsule"
   }
+}
+
+
+
+# ============ SUBNETS =====================================
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id = aws_vpc.kodecapsule-vpc.id
+  cidr_block = "10.0.1.0"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+   tags = {
+    Name = "kodecapsule-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+    vpc_id = aws_vpc.kodecapsule-vpc.id
+    cidr_block = "10.0.0.2.0"
+    availability_zone = "use-east-1b"
+
+     tags = {
+    Name = "kodecapsule-public-subnet"
+  }
+  
+}
+
+
+# ============ ROUTE TABLE & IGW=====================================
+
+resource "aws_internet_gateway" "kodecapsule-igw" {
+
+    vpc_id = aws_vpc.kodecapsule-vpc.id
+
+      tags = {
+    Name = "kodecapsule-igw"
+  }
+
+  
+}
+
+resource "aws_route_table" "public-rt" {
+    vpc_id = aws_vpc.kodecapsule-vpc.id
+
+    route = {
+        cidr_block="0.0.0.0"
+        gateway_id = aws_internet_gateway.kodecapsule-igw.id
+    }
+}
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public-rt.id
+  subnet_id = aws_subnet.public-subnet.id
 }
